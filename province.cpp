50be58b4d996bd3f1aa01eb33fd7b71ced0e7e4f
olikr3 <oliver.kremser@gmail.com>
2025-04-20 19:34:40 +0200

    implemented basic province structure

--- province.cpp
+++ province.cpp
@@ -1,8 +1,24 @@
-
-struct Province {
+class Province {
+private:
     int id;
     std::string name;
-    std::vector<int> nbors;
-    
+    std::vector<int> nborIds;
     float population;
-}
+    int ownerId; // faction that owns province
+
+public:
+    Province(int id, const std::string& name, float population)
+        : id(id), name(name), population(population), ownerId(-1) {}
+
+    int getId() const { return id; }
+    const std::string& getName() const { return name; }
+    float getPopulation() const { return population; }
+    int getOwnerId() const { return ownerId; }
+
+    void setOwnerId(int newOwner) { ownerId = newOwner; }
+    void addNeighbor(int neighborId) { neighborIds.push_back(neighborId); }
+
+    bool isNeighbor(int provinceId) const {
+        return std::find(neighborIds.begin(), neighborIds.end(), provinceId) != neighborIds.end();
+    }
+};

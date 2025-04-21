3269345a3cce60c4b66e9f9bc03cb60739c20cc0
olikr3 <oliver.kremser@gmail.com>
2025-04-21 15:29:59 +0200

    changes to class

--- include/province.hpp
+++ include/province.hpp
@@ -2,21 +2,38 @@ class Province {
 private:
     int id;
     std::string name;
-    std::vector<int> neighborIds;
-    float population;
-    int ownerId;
+    std::vector<int> neighbor_ids;
+    int owner_id;
+
+    //population-related
+    int population_count;
+    float population_satisfaction_factor;
+
+    // economy-related
+    int mill_count;
+    int farm_count;
+    float production_factor;
+
+    // units in tons
+    float steel_production;
+    float coal_production;
+    float grain_production;
+
+    // military-related
+    int num_soldiers;
+    float soldier_strength_factor; // multiply with base strength
 
 public:
     Province(int id, const std::string& name, float population)
 
-    int getId() const;
-    const std::string& getName() const;
-    float getPopulation() const;
-    int getOwnerId() const;
+    int get_id() const;
+    const std::string& get_name() const;
+    float get_population() const;
+    int get_owner_id() const;
 
-    void setOwnerId(int newOwner);
-    void addNeighbor(int neighborId);
+    void set_owner_id(int new_owner);
+    void add_neighbor(int neighbor_id);
 
-    bool isNeighbor(int provinceId) const;
+    bool is_neighbor(int province_id) const;
 };
 
